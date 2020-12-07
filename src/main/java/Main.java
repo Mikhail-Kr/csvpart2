70fd39b38a6c3c902d3214ebe0a7294b952d4bec
Владислав Люминарский <Vladislav.Lyuminarskiy@gmail.com>
2020-12-07 21:59:42 +0400

    Переименован параметр методов.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,46 +4,46 @@ import org.jetbrains.annotations.Nullable;
 public class Main {
   /**
    * Создаёт новую запись о пользователе {@code user}
-   * в файле {@code fileName} формата CSV.
+   * в файле {@code filePath} формата CSV.
    *
-   * @param fileName имя файла.
+   * @param filePath путь к файлу.
    * @param user пользователь.
    */
-  public static void create(@NotNull String fileName, @NotNull User user) {}
+  public static void create(@NotNull String filePath, @NotNull User user) {}
 
   /**
-   * Считывает из файла {@code fileName} формата CSV запись о пользователе
+   * Считывает из файла {@code filePath} формата CSV запись о пользователе
    * с идентификатором {@code id}. Возвращает {@code null}, если записи
    * с таким идентификатором не существует.
    *
-   * @param fileName имя файла.
+   * @param filePath путь к файлу.
    * @param id идентификатор записи о пользователе.
    * @return Пользователь или {@code null}.
    */
-  public static @Nullable User read(@NotNull String fileName, int id) {
+  public static @Nullable User read(@NotNull String filePath, int id) {
     return null;
   }
 
   /**
-   * Обновляет в файле {@code fileName} формата CSV информацию в записи
+   * Обновляет в файле {@code filePath} формата CSV информацию в записи
    * о пользователе с идентификатором {@code id}.
    *
-   * @param fileName имя файла.
+   * @param filePath путь к файлу.
    * @param id идентификатор записи о пользователе.
    * @param user пользователь.
    * @throws java.util.NoSuchElementException запись о пользователе
    *     с идентификатором {@code id} не существует.
    */
-  public static void update(@NotNull String fileName, int id, @NotNull User user) {}
+  public static void update(@NotNull String filePath, int id, @NotNull User user) {}
 
   /**
-   * Удаляет в файле {@code fileName} формата CSV запись
+   * Удаляет в файле {@code filePath} формата CSV запись
    * о пользователе с идентификатором {@code id}.
    *
-   * @param fileName имя файла.
+   * @param filePath путь к файлу.
    * @param id идентификатор записи о пользователе.
    * @throws java.util.NoSuchElementException запись о пользователе
    *     с идентификатором {@code id} не существует.
    */
-  public static void delete(@NotNull String fileName, int id) {}
+  public static void delete(@NotNull String filePath, int id) {}
 }
