b820d66cc323e8092ea7ceeae0cb8f65677b6025
Владислав Люминарский <Vladislav.Lyuminarskiy@gmail.com>
2020-12-18 16:22:04 +0400

    Обновлены модульные тесты.

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -21,13 +21,6 @@ class MainTest {
   void setUp() throws IOException {
     Files.deleteIfExists(filePath);
     Files.createFile(filePath);
-    Files.write(filePath,
-        List.of(
-          "1,Murphy,Aileen,Deborah,38",
-          "3,Norton,Robert,\"\",12"
-        ),
-        StandardCharsets.UTF_8
-    );
   }
 
   @AfterEach
@@ -36,17 +29,94 @@ class MainTest {
   }
 
   @Test
-  void create_shouldSucceed() throws IOException {
+  void create_fileIsNotEmpty_shouldSucceed() throws IOException {
+    Files.write(filePath,
+        List.of(
+          "1,Murphy,Aileen,Deborah,38",
+          "4,Norton,Robert,\"\",12"
+        ),
+        StandardCharsets.UTF_8
+    );
+
     List.of(
       new User(15, "Dawson", "Augusta"),
-      new User(25, "Ford", "Joseph", "Nicholas")
+      new User(25, "Ford", "Joseph", "Nicholas"),
+      new User(18, "Lambert", "Edward")
     ).forEach(user -> Main.create(filePath.toString(), user));
 
     assertEquals(
         String.join("\n", List.of(
           "1,Murphy,Aileen,Deborah,38",
           "2,Dawson,Augusta,\"\",15",
-          "3,Norton,Robert,\"\",12",
+          "3,Ford,Joseph,Nicholas,25",
+          "4,Norton,Robert,\"\",12",
+          "5,Lambert,Edward,\"\",18"
+        )),
+        String.join("\n", Files.readAllLines(filePath))
+    );
+  }
+
+  @Test
+  void create_fileIsEmpty_shouldSucceed() throws IOException {
+    List.of(
+      new User(15, "Dawson", "Augusta"),
+      new User(25, "Ford", "Joseph", "Nicholas"),
+      new User(18, "Lambert", "Edward")
+    ).forEach(user -> Main.create(filePath.toString(), user));
+
+    assertEquals(
+        String.join("\n", List.of(
+          "1,Dawson,Augusta,\"\",15",
+          "2,Ford,Joseph,Nicholas,25",
+          "3,Lambert,Edward,\"\",18"
+        )),
+        String.join("\n", Files.readAllLines(filePath))
+    );
+  }
+
+  @Test
+  void read_fileIsNotEmpty_shouldReturnUser() throws IOException {
+    Files.write(filePath,
+        List.of(
+          "1,Murphy,Aileen,Deborah,38",
+          "4,Norton,Robert,\"\",12"
+        ),
+        StandardCharsets.UTF_8
+    );
+
+    assertEquals(
+        new User(12, "Norton", "Robert"),
+        Main.read(filePath.toString(), 4)
+    );
+  }
+
+  @Test
+  void read_fileIsNotEmpty_shouldReturnNull() throws IOException {
+    Files.write(filePath, List.of("1,Murphy,Aileen,Deborah,38"), StandardCharsets.UTF_8);
+    assertNull(Main.read(filePath.toString(), 10));
+  }
+
+  @Test
+  void read_fileIsEmpty_shouldReturnNull() {
+    assertNull(Main.read(filePath.toString(), 10));
+  }
+
+  @Test
+  void update_fileIsNotEmpty_shouldSucceed() throws IOException {
+    Files.write(filePath,
+        List.of(
+          "1,Murphy,Aileen,Deborah,38",
+          "4,Norton,Robert,\"\",12"
+        ),
+        StandardCharsets.UTF_8
+    );
+
+    Main.update(filePath.toString(), 4,
+        new User(25, "Ford", "Joseph", "Nicholas"));
+
+    assertEquals(
+        String.join("\n", List.of(
+          "1,Murphy,Aileen,Deborah,38",
           "4,Ford,Joseph,Nicholas,25"
         )),
         String.join("\n", Files.readAllLines(filePath))
@@ -54,49 +124,58 @@ class MainTest {
   }
 
   @Test
-  void read_shouldReturnUser() {
-    assertEquals(
-        new User(12, "Norton", "Robert"),
-        Main.read(filePath.toString(), 3)
-    );
-  }
-
-  @Test
-  void read_shouldReturnNull() {
-    assertNull(Main.read(filePath.toString(), 10));
-  }
-
-  @Test
-  void update_shouldSucceed() throws IOException {
-    Main.update(filePath.toString(), 3,
-        new User(25, "Ford", "Joseph", "Nicholas"));
-
-    assertEquals(
-        String.join("\n", List.of(
+  void update_fileIsNotEmpty_shouldThrowNoSuchElementException() throws IOException {
+    Files.write(filePath,
+        List.of(
           "1,Murphy,Aileen,Deborah,38",
-          "3,Ford,Joseph,Nicholas,25"
-        )),
-        String.join("\n", Files.readAllLines(filePath))
+          "4,Norton,Robert,\"\",12"
+        ),
+        StandardCharsets.UTF_8
     );
-  }
 
-  @Test
-  void update_shouldThrow() {
     assertThrows(NoSuchElementException.class, () -> Main.update(filePath.toString(), 10,
         new User(12, "Norton", "Robert")));
   }
 
   @Test
-  void delete_shouldSucceed() throws IOException {
+  void update_fileIsEmpty_shouldThrowNoSuchElementException() {
+    assertThrows(NoSuchElementException.class, () -> Main.update(filePath.toString(), 10,
+        new User(12, "Norton", "Robert")));
+  }
+
+  @Test
+  void delete_fileIsNotEmpty_shouldSucceed() throws IOException {
+    Files.write(filePath,
+        List.of(
+          "1,Murphy,Aileen,Deborah,38",
+          "4,Norton,Robert,\"\",12"
+        ),
+        StandardCharsets.UTF_8
+    );
+
     Main.delete(filePath.toString(), 1);
+
     assertEquals(
-        "3,Norton,Robert,\"\",12",
+        "4,Norton,Robert,\"\",12",
         String.join("\n", Files.readAllLines(filePath))
     );
   }
 
   @Test
-  void delete_shouldThrow() {
+  void delete_fileIsNotEmpty_shouldThrowNoSuchElementException() throws IOException {
+    Files.write(filePath,
+        List.of(
+          "1,Murphy,Aileen,Deborah,38",
+          "4,Norton,Robert,\"\",12"
+        ),
+        StandardCharsets.UTF_8
+    );
+
+    assertThrows(NoSuchElementException.class, () -> Main.delete(filePath.toString(), 10));
+  }
+
+  @Test
+  void delete_fileIsEmpty_shouldThrowNoSuchElementException() {
     assertThrows(NoSuchElementException.class, () -> Main.delete(filePath.toString(), 10));
   }
 }
